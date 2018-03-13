aae9b6941c24b3098262c34e090f8bfb5b4131ec
Sara <sara@xandium.net>
2018-03-13 18:40:53 -0400

    Added base objects for remaining gateway messages

--- include/aegis/events/guild_integrations_update.hpp
+++ include/aegis/events/guild_integrations_update.hpp
@@ -1,3 +1,51 @@
+//
+// guild_integrations_update.hpp
+// aegis.cpp
+//
+// Copyright (c) 2017 Sara W (sara at xandium dot net)
+//
+// This file is part of aegis.cpp .
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 #pragma once
 
-//TODO:
+#include "../config.hpp"
+
+
+
+namespace aegiscpp
+{
+
+class member;
+class shard;
+class aegis;
+
+/**\todo Needs documentation
+*/
+struct guild_integrations_update
+{
+};
+
+/**\todo Needs documentation
+*/
+inline void from_json(const nlohmann::json& j, guild_integrations_update& m)
+{
+}
+
+}
