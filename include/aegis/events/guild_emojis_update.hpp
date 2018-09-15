ae2c19c69b271908586152674fb2e99f2c712b32
Sharon <sharon@aegisbot.io>
2018-09-15 06:28:04 -0400

    Added GUILD_EMOJIS_UPDATE event object handling

--- include/aegis/events/guild_emojis_update.hpp
+++ include/aegis/events/guild_emojis_update.hpp
@@ -27,12 +27,18 @@ struct guild_emojis_update
 {
     shards::shard * _shard = nullptr; /**< Pointer to shard object this message came from */
     core * bot = nullptr; /**< Pointer to the main bot object */
+    snowflake guild_id;
+    std::vector<objects::emoji> emojis; /**<\todo Needs documentation */
 };
 
 /**\todo Needs documentation
  */
 inline void from_json(const nlohmann::json& j, guild_emojis_update& m)
 {
+    m.guild_id = j["guild_id"];
+    if (j.count("emojis") && !j["emojis"].is_null())
+        for (const auto & _emoji : j["emojis"])
+            m.emojis.push_back(_emoji);
 }
 
 }
