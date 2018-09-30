f5726b3bb1755e1a66ff79882ec320f3f06d4940
Sharon <sharon@aegisbot.io>
2018-09-30 09:32:01 -0400

    Added channel_pins_update handling

--- include/aegis/events/channel_pins_update.hpp
+++ include/aegis/events/channel_pins_update.hpp
@@ -26,11 +26,16 @@ struct channel_pins_update
 {
     shards::shard * _shard = nullptr; /**< Pointer to shard object this message came from */
     core * bot = nullptr; /**< Pointer to the main bot object */
+    snowflake channel_id; /**< Snowflake of channel */
+    std::string last_pin_timestamp; /**< ISO8601 timestamp of most recent pinned message */
 };
 
 /// \cond TEMPLATES
 inline void from_json(const nlohmann::json& j, channel_pins_update& m)
 {
+    m.channel_id = j["channel_id"];
+    if (j.count("last_pin_timestamp") && !j["last_pin_timestamp"].is_null())
+        m.last_pin_timestamp = j["last_pin_timestamp"];
 }
 /// \endcond
 
