78a31b7dfef3e2472f0578d5ea523987372c5e1b
Sharon <sharon@aegisbot.io>
2018-10-16 02:49:42 -0400

    Changed embed to fluent-style interface

--- include/aegis/objects/embed.hpp
+++ include/aegis/objects/embed.hpp
@@ -41,119 +41,128 @@ public:
      * @param value Text to be shown within field
      * @param is_inline Sets whether the field is inline
      */
-    void add_field(const std::string & name, const std::string & value, bool is_inline = false)
+    embed & fields(std::vector<objects::field> flds)
     {
-        fields.emplace_back(name, value, is_inline);
+        _fields = flds;
+        return *this;
     }
     /// Sets the title of the embed
     /**
      * @param str Title to set
      */
-    void set_title(const std::string & str)
+    embed & title(const std::string & str)
     {
-        title = str;
+        _title = str;
+        return *this;
     }
     /// Sets the footer of the embed
     /**
      * @param str Footer to set
      */
-    void set_footer(const footer ftr)
+    embed & footer(const footer ftr)
     {
-        footer_ = ftr;
+        _footer = ftr;
+        return *this;
     }
     /// Sets the description of the embed
     /**
      * @param str Description to set
      */
-    void set_description(const std::string & str)
+    embed & description(const std::string & str)
     {
-        description = str;
+        _description = str;
+        return *this;
     }
     /// Sets the url of the embed
     /**
      * @param str Url to set
      */
-    void set_url(const std::string & str)
+    embed & url(std::string & str)
     {
-        url = str;
+        _url = str;
+        return *this;
     }
     /// Sets the timestamp of the embed
     /**
      * @param str Timestamp to set
      */
-    void set_timestamp(const std::string & str)
+    embed & timestamp(const std::string & str)
     {
-        timestamp = str;
+        _timestamp = str;
+        return *this;
     }
     /// Sets the color of the embed
     /**
      * @param clr Color to set
      */
-    void set_color(const int32_t clr)
+    embed & color(const int32_t clr)
     {
-        color = clr;
+        _color = clr;
+        return *this;
     }
     // Combined Limit: 6000
-    std::string title; /**<\todo Needs documentation */ // Limit: 256
-    std::string type; /**<\todo Needs documentation */
-    std::string description; /**<\todo Needs documentation */ // Limit: 2048
-    std::string url; /**<\todo Needs documentation */
-    std::string timestamp; /**<\todo Needs documentation */
-    int32_t color = 0; /**<\todo Needs documentation */
-    footer footer_; /**<\todo Needs documentation */ // Limit: 2048
-    image image_; /**<\todo Needs documentation */
-    thumbnail thumbnail_; /**<\todo Needs documentation */
-    video video_; /**<\todo Needs documentation */
-    provider provider_; /**<\todo Needs documentation */
-    std::vector<field> fields; /**<\todo Needs documentation */ // Limit: 25 name:256 value:1024
+    std::string _title; /**<\todo Needs documentation */ // Limit: 256
+    std::string _type; /**<\todo Needs documentation */
+    std::string _description; /**<\todo Needs documentation */ // Limit: 2048
+    std::string _url; /**<\todo Needs documentation */
+    std::string _timestamp; /**<\todo Needs documentation */
+    int32_t _color = 0; /**<\todo Needs documentation */
+    objects::footer _footer; /**<\todo Needs documentation */ // Limit: 2048
+    objects::image _image; /**<\todo Needs documentation */
+    objects::thumbnail _thumbnail; /**<\todo Needs documentation */
+    objects::video _video; /**<\todo Needs documentation */
+    objects::provider _provider; /**<\todo Needs documentation */
+    std::vector<objects::field> _fields; /**<\todo Needs documentation */ // Limit: 25 name:256 value:1024
+    friend void from_json(const nlohmann::json& j, embed& m);
+    friend void to_json(nlohmann::json& j, const embed& m);
 };
 
 /// \cond TEMPLATES
 inline void from_json(const nlohmann::json& j, embed& m)
 {
     if (j.count("title") && !j["title"].is_null())
-        m.title = j["title"];
+        m._title = j["title"].get<std::string>();
     if (j.count("type"))
-        m.type = j["type"];
+        m._type = j["type"].get<std::string>();
     if (j.count("description") && !j["description"].is_null())
-        m.description = j["description"];
+        m._description = j["description"].get<std::string>();
     if (j.count("url") && !j["url"].is_null())
-        m.url = j["url"];
+        m._url = j["url"].get<std::string>();
     if (j.count("timestamp") && !j["timestamp"].is_null())
-        m.timestamp = j["timestamp"];
+        m._timestamp = j["timestamp"].get<std::string>();
     if (j.count("color") && !j["color"].is_null())
-        m.color = j["color"];
+        m._color = j["color"];
     if (j.count("footer") && !j["footer"].is_null())
-        m.footer_ = j["footer"];
+        m._footer = j["footer"];
     if (j.count("image") && !j["image"].is_null())
-        m.image_ = j["image"];
+        m._image = j["image"];
     if (j.count("thumbnail") && !j["thumbnail"].is_null())
-        m.thumbnail_ = j["thumbnail"];
+        m._thumbnail = j["thumbnail"];
     if (j.count("video") && !j["video"].is_null())
-        m.video_ = j["video"];
+        m._video = j["video"];
     if (j.count("provider") && !j["provider"].is_null())
-        m.provider_ = j["provider"];
+        m._provider = j["provider"];
     if (j.count("fields") && !j["fields"].is_null())
         for (const auto & _field : j["fields"])
-            m.fields.push_back(_field);
+            m._fields.push_back(_field);
 }
 /// \endcond
 
 /// \cond TEMPLATES
 inline void to_json(nlohmann::json& j, const embed& m)
 {
-    j["title"] = m.title;
-    j["type"] = m.type;
-    j["description"] = m.description;
-    j["url"] = m.url;
-    j["timestamp"] = m.timestamp;
-    j["color"] = m.color;
-    j["footer"] = m.footer_;
-    j["image"] = m.image_;
-    j["thumbnail"] = m.thumbnail_;
-    j["video"] = m.video_;
-    j["provider"] = m.provider_;
-    for (const auto & _field : m.fields)
+    j["title"] = m._title;
+    j["type"] = m._type;
+    j["description"] = m._description;
+    j["url"] = m._url;
+    j["timestamp"] = m._timestamp;
+    j["color"] = m._color;
+    j["footer"] = m._footer;
+    j["image"] = m._image;
+    j["thumbnail"] = m._thumbnail;
+    j["video"] = m._video;
+    j["provider"] = m._provider;
+    for (const auto & _field : m._fields)
         j["fields"].push_back(_field);
 }
 /// \endcond
