6e2fc6a7eebf7aafec78fa38d3c332fac4efd675
Craig Edwards <craigedwards@brainbox.cc>
2019-11-01 20:22:59 +0000

    remove do...while in preference for naked braces

--- include/aegis/impl/core.cpp
+++ include/aegis/impl/core.cpp
@@ -1562,11 +1562,11 @@ AEGIS_DECL void core::ws_channel_delete(const json & result, shards::shard * _sh
         * Don't keep this lock in scope when we reach it, otherwise bad stuff
         * will happen. - CraigE
         */
-       do {
+       {
                 std::unique_lock<shared_mutex> l2(_channel_m, std::defer_lock);
                 std::lock(l, l2);
                 _guild->_remove_channel(channel_id);
-       } while (false);
+       }
         remove_channel(channel_id);
     }
 
