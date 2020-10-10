0b8ee72fe11c786bd614cc0146fb1cac51120ada
Lohkdesgds <lohk.lohkdesgds@gmail.com>
2020-10-10 01:03:43 -0300

    Updated Future with some new stuff and small fixes.

--- include/lsw/future_mod.h
+++ include/lsw/future_mod.h
@@ -1,8 +1,16 @@
 #pragma once
 
+// easier way to use anywhere else
+#define USE_STD_FULLY
+
+#ifndef USE_STD_FULLY
+#include "..\..\Handling\Abort\abort.h"
+#endif
+
 #include <future>
 #include <functional>
 #include <any>
+
 
 #define TIMEOUT_MS 20000 // timeout if get() gets stuck for too long
 
@@ -16,24 +24,44 @@ namespace LSW {
 				bool promise_ran = false, promise_passed = false, f_ran = false;
 				std::mutex m;
 			public:
+				/// <summary>
+				/// <para>Sets the function (then function).</para>
+				/// </summary>
+				/// <param name="{std::function}">The then() function itself.</param>
 				void set(std::function<void(T)> nf) {
 					std::lock_guard<std::mutex> luck(m);
 					f = nf;
 				}
+
+				/// <summary>
+				/// <para>Gets if it has task to do (based on what have been done so far).</para>
+				/// </summary>
+				/// <returns>{bool} True if has task.</returns>
 				bool has_future_task() {
 					std::lock_guard<std::mutex> luck(m);
 					return (promise_passed && !promise_ran) && f && !f_ran;
 				}
+
+				/// <summary>
+				/// <para>Do the task as future/promise if needed.</para>
+				/// </summary>
+				/// <param name="{Q}">The value to be set.</param>
+				/// <param name="{bool}">Is this promise?</param>
 				template<typename Q = T, std::enable_if_t<!std::is_void_v<Q>, int> = 0>
-				void run_if_not_yet(Q v, const bool is_this_promisse) {
+				void run_if_not_yet(Q v, const bool is_this_promise) {
 					std::lock_guard<std::mutex> luck(m);
 					if (f && !f_ran) {
 						f(v);
 						f_ran = true;
-						promise_ran |= is_this_promisse;
+						promise_ran |= is_this_promise;
 					}
-					promise_passed |= is_this_promisse;
+					promise_passed |= is_this_promise;
 				}
+
+				/// <summary>
+				/// <para>Do the task as future/promise if needed. (void edition)</para>
+				/// </summary>
+				/// <param name="{bool}">Is this promise?</param>
 				template<typename Q = T, std::enable_if_t<std::is_void_v<Q>, int> = 0>
 				void run_if_not_yet(const bool is_this_promisse) {
 					std::lock_guard<std::mutex> luck(m);
@@ -58,7 +86,10 @@ namespace LSW {
 				~fake() { if (del) del(); }
 			};
 
-
+			/// <summary>
+			/// <para>The Future holds or is about to hold a value in the future.</para>
+			/// <para>You can use this to let a thread set the value assynchronously while doing other stuff.</para>
+			/// </summary>
 			template<typename T = void>
 			class Future : std::future<T> {
 				std::shared_ptr<then_block<T>> next = std::make_shared<then_block<T>>(); // internally a Promise<any> (thistype)
@@ -70,48 +101,152 @@ namespace LSW {
 				template<typename Q = T, std::enable_if_t<std::is_void_v<Q>, int> = 0>
 				void _end();
 			public:
+
+				/// <summary>
+				/// <para>Move constructor.</para>
+				/// </summary>
+				/// <param name="{std::future}">The future to move.</param>
 				Future(std::future<T>&& f) : std::future<T>(std::move(f)) { }
+
+				/// <summary>
+				/// <para>Move constructor.</para>
+				/// </summary>
+				/// <param name="{Future}">The Future to move.</param>
 				template<typename Q = T, std::enable_if_t<!std::is_void_v<Q>, int> = 0>
 				Future(Future<T>&& f) noexcept : std::future<T>(std::move(f)) { next = f.next; *later_value.var = *f.later_value.var; got_value_already = f.got_value_already; }
+
+				/// <summary>
+				/// <para>Move constructor.</para>
+				/// </summary>
+				/// <param name="{Future}">The Future to move.</param>
 				template<typename Q = T, std::enable_if_t<std::is_void_v<Q>, int> = 0>
 				Future(Future<T>&& f) noexcept : std::future<T>(std::move(f)) { next = f.next; got_value_already = f.got_value_already; }
+
 				~Future() { _end<T>(); }
 
+
+				/// <summary>
+				/// <para>Assign copy.</para>
+				/// </summary>
+				/// <param name="{std::future}">The future to move.</param>
 				void operator=(std::future<T>&& f) { std::future<T>::operator=(std::move(f)); }
+
+				/// <summary>
+				/// <para>Assign copy.</para>
+				/// </summary>
+				/// <param name="{Future}">The Future to move.</param>
 				void operator=(Future<T>&& f) { std::future<T>::operator=(std::move(f)); next = f.next; *later_value.var = *f.later_value.var; got_value_already = f.got_value_already; }
 
+				/// <summary>
+				/// <para>Gets the value (if it is set) or waits (with timeout) for it to be set.</para>
+				/// <para>* if you want no timeout, wait() before get().</para>
+				/// </summary>
+				/// <returns>{T} The value.</returns>
 				template<typename Q = T, std::enable_if_t<!std::is_void_v<Q>, int> = 0>
 				T get();
+
+				/// <summary>
+				/// <para>Gets if has set or waits for signal (with timeout).</para>
+				/// <para>* if you want no timeout, wait() before get().</para>
+				/// </summary>
 				template<typename Q = T, std::enable_if_t<std::is_void_v<Q>, int> = 0>
 				void get();
-				// wait is a get with no timeout. You can get() later.
+
+				/// <summary>
+				/// <para>Waits for the signal to get the value.</para>
+				/// </summary>
 				template<typename Q = T, std::enable_if_t<!std::is_void_v<Q>, int> = 0>
 				void wait();
+
+				/// <summary>
+				/// <para>Waits for the signal.</para>
+				/// </summary>
 				template<typename Q = T, std::enable_if_t<std::is_void_v<Q>, int> = 0>
 				void wait();
+
+				/// <summary>
+				/// <para>Gets if the variable is set.</para>
+				/// </summary>
+				/// <param name="{unsigned}">Max time to wait for status.</param>
+				/// <returns>{bool} True if available to get().</returns>
 				// if got, true, if not and if it has to wait, false.
 				bool get_ready(unsigned = 0);
 
-				template<typename Q = void, std::enable_if_t<!std::is_void_v<Q> && !std::is_void_v<T>, int> = 0>
+
+				/// <summary>
+				/// <para>Sets a function to be called after value is set.</para>
+				/// <para>The type the function returns is the Future type this function returns.</para>
+				/// </summary>
+				/// <param name="{std::function}">A function that has T as argument and returns a type Q.</param>
+				/// <returns>{Future} A Future of type Q.</returns>
+				template<typename Q = T, std::enable_if_t<!std::is_void_v<Q> && !std::is_void_v<T>, int> = 0>
 				Future<Q> then(std::function<Q(T)>);
-				template<typename Q = void, std::enable_if_t<std::is_void_v<Q> && !std::is_void_v<T>, int> = 0>
-				Future<Q> then(std::function<void(T)>);
-				template<typename Q = void, std::enable_if_t<!std::is_void_v<Q>&& std::is_void_v<T>, int> = 0>
-				Future<Q> then(std::function<Q(T)>);
-				template<typename Q = void, std::enable_if_t<std::is_void_v<Q>&& std::is_void_v<T>, int> = 0>
+
+				/// <summary>
+				/// <para>Sets a function to be called after value is set.</para>
+				/// <para>The type the function returns is the Future type this function returns.</para>
+				/// </summary>
+				/// <param name="{std::function}">A function that has T as argument and returns a type VOID.</param>
+				/// <returns>{Future} A Future of type VOID.</returns>
+				template<typename Q = T, std::enable_if_t<std::is_void_v<Q> && !std::is_void_v<T>, int> = 0>
 				Future<Q> then(std::function<void(T)>);
 
+				/// <summary>
+				/// <para>Sets a function to be called after value is set.</para>
+				/// <para>The type the function returns is the Future type this function returns.</para>
+				/// </summary>
+				/// <param name="{std::function}">A function that has NO arguments and returns a type Q.</param>
+				/// <returns>{Future} A Future of type Q.</returns>
+				template<typename Q = T, std::enable_if_t<!std::is_void_v<Q> && std::is_void_v<T>, int> = 0>
+				Future<Q> then(std::function<Q(T)>);
+
+				/// <summary>
+				/// <para>Sets a function to be called after value is set.</para>
+				/// <para>The type the function returns is the Future type this function returns.</para>
+				/// </summary>
+				/// <param name="{std::function}">A function that has NO arguments and returns a type VOID.</param>
+				/// <returns>{Future} A Future of type VOID.</returns>
+				template<typename Q = T, std::enable_if_t<std::is_void_v<Q> && std::is_void_v<T>, int> = 0>
+				Future<Q> then(std::function<void(T)>);
+
+				/// <summary>
+				/// <para>Sets a function to be called after value is set.</para>
+				/// <para>The type the function in this case is forced to be the VOID VOID.</para>
+				/// </summary>
+				/// <param name="{std::function}">A function that has NO arguments and returns a type VOID.</param>
+				/// <returns>{Future} A Future of type VOID.</returns>
+				template<typename Q = T, typename Void = void, std::enable_if_t<!std::is_void_v<Q> && std::is_void_v<Void>, int> = 0>
+				Future<void> then(std::function<void(Void)>);
+
+
+				/// <summary>
+				/// <para>Internal method because of templates not being friends of others from the same class, but not same type.</para>
+				/// </summary>
+				/// <param name="{std::function}">The function it is linked to. (forward setting)</param>
+				/// <param name="{std::shared_ptr}">Copy of back then_block.</param>
 				template<typename R, std::enable_if_t<!std::is_void_v<R>, int> = 0>
 				void _set_next(std::function<void(T)>, std::shared_ptr<then_block<R>>);
+
+				/// <summary>
+				/// <para>Internal method because of templates not being friends of others from the same class, but not same type.</para>
+				/// </summary>
+				/// <param name="{std::function}">The function it is linked to. (forward setting)</param>
+				/// <param name="{std::shared_ptr}">Copy of back then_block.</param>
 				template<typename R, std::enable_if_t<std::is_void_v<R>, int> = 0>
 				void _set_next(std::function<void(T)>, std::shared_ptr<then_block<R>>);
 
+				/// <summary>
+				/// <para>Internal method to get internal then (because different types are not friends).</para>
+				/// </summary>
+				/// <returns>{std::shared_ptr} The then_block block.</returns>
 				std::shared_ptr<then_block<T>> _get_then();
 			};
 
 
 
-
+			/// <summary>
+			/// <para>Promise is the one that can generate a Future and set its value later.</para>
+			/// </summary>
 			template<typename T = void>
 			class Promise : std::promise<T> {
 				bool got_future = false;
@@ -120,30 +255,80 @@ namespace LSW {
 				bool got_future_once = false;
 				bool set_already_skip = false;
 			public:
-				Promise() : std::promise<T>() {}
-				// initializes with the function that it will run (that gives the final result)
-				Promise(std::function<T(void)> nf) : std::promise<T>() {
-					task_to_do = nf;
-				}
+				Promise(const Promise&) = delete;
 
+				/// <summary>
+				/// <para>Default constructor.</para>
+				/// </summary>
+				Promise() : std::promise<T>() {}
+
+				/// <summary>
+				/// <para>Move constructor.</para>
+				/// </summary>
+				/// <param name="{Promise}">Promise to move.</param>
+				Promise(Promise&&);
+
+				/// <summary>
+				/// <para>Move.</para>
+				/// </summary>
+				/// <param name="{Promise}">Promise to move.</param>
+				void operator=(Promise&&);
+
+				/// <summary>
+				/// <para>Constructor that sets up internal work function directly.</para>
+				/// </summary>
+				/// <param name="{std::function}">The function it will run when work() is called.</param>
+				Promise(std::function<T(void)>);
+
+				/// <summary>
+				/// <para>Gets the Future associated to this Promise.</para>
+				/// <para>Can only be called once per object.</para>
+				/// </summary>
+				/// <returns>{Future} The Future.</returns>
 				Future<T> get_future();
 
-				// work function if there's one. It won't let it run two times.
+				/// <summary>
+				/// <para>Works (runs) the function set and sets as tasked.</para>
+				/// <para>The value is set to the Future.</para>
+				/// </summary>
 				template<typename Q = T, std::enable_if_t<!std::is_void_v<Q>, int> = 0>
 				void work();
-				// work function if there's one. It won't let it run two times.
+
+				/// <summary>
+				/// <para>Works (runs) the function set and sets as tasked.</para>
+				/// <para>The Future will know the task has been done.</para>
+				/// </summary>
 				template<typename Q = T, std::enable_if_t<std::is_void_v<Q>, int> = 0>
 				void work();
 
+				/// <summary>
+				/// <para>Has the value been set already?</para>
+				/// </summary>
+				/// <returns>{bool} True if has worked and/or the value has been set.</returns>
 				bool has_set();
 
+				/// <summary>
+				/// <para>Sets as done and notifies Future.</para>
+				/// </summary>
+				/// <returns>{bool} True if it was set (not set before this call).</returns>
 				template<typename Q = T>
 				typename std::enable_if_t<std::is_void_v<Q>, bool> set_value();
+
+				/// <summary>
+				/// <para>Sets value and notifies Future.</para>
+				/// </summary>
+				/// <param name="{T}">The value to be set.</param>
+				/// <returns>{bool} True if it was set (not set before this call).</returns>
 				template<typename Q = T>
 				typename std::enable_if_t<!std::is_void_v<Q>, bool> set_value(Q);
 			};
 
-
+			/// <summary>
+			/// <para>In some weird cases you might need this, but it is not recommended, because this is not multithread.</para>
+			/// <para>This sets a Future like a defined value already if you need to "cast" a value to a Future directly.</para>
+			/// </summary>
+			/// <param name="{T}">The value the Future will hold already.</param>
+			/// <returns>{Future} The future of type T with the value you've set.</returns>
 			template<typename T>
 			Future<T> fake_future(T set) {
 				Promise<T> pr;
@@ -173,7 +358,11 @@ namespace LSW {
 			template<typename Q, std::enable_if_t<!std::is_void_v<Q>, int>>
 			T Future<T>::get() {
 				if (std::future<T>::valid()) {
-					if (!get_ready(TIMEOUT_MS)) throw std::exception("Blocked for too long! Variable can't be set!"); // if invalid after 20 sec, cancel yeah?
+#ifndef USE_STD_FULLY
+					if (!get_ready(TIMEOUT_MS)) throw Handling::Abort(__FUNCSIG__, "Blocked for too long! Variable can't be set!", Handling::abort::abort_level::GIVEUP); // if invalid after 10 sec, cancel?
+#else
+					if (!get_ready(TIMEOUT_MS)) throw std::exception("Blocked for too long! Variable can't be set!"); // if invalid after 10 sec, cancel?
+#endif
 					*later_value.var = std::future<T>::get();
 					if (next->has_future_task()) next->run_if_not_yet(*later_value.var, false);
 					got_value_already = true;
@@ -184,7 +373,11 @@ namespace LSW {
 			template<typename Q, std::enable_if_t<std::is_void_v<Q>, int>>
 			void Future<T>::get() {
 				if (std::future<T>::valid()) {
-					if (!get_ready(TIMEOUT_MS)) throw std::exception("Blocked for too long! Variable can't be set!"); // if invalid after 20 sec, cancel yeah?
+#ifndef USE_STD_FULLY
+					if (!get_ready(TIMEOUT_MS)) throw Handling::Abort(__FUNCSIG__, "Blocked for too long! Variable can't be set!", Handling::abort::abort_level::GIVEUP); // if invalid after 10 sec, cancel?
+#else
+					if (!get_ready(TIMEOUT_MS)) throw std::exception("Blocked for too long! Variable can't be set!"); // if invalid after 10 sec, cancel?
+#endif
 					std::future<T>::get();
 					if (next->has_future_task()) next->run_if_not_yet(false);
 					got_value_already = true;
@@ -199,7 +392,6 @@ namespace LSW {
 					if (next->has_future_task()) next->run_if_not_yet(*later_value.var, false);
 					got_value_already = true;
 				}
-				return *later_value.var;
 			}
 			template<typename T>
 			template<typename Q, std::enable_if_t<std::is_void_v<Q>, int>>
@@ -267,6 +459,19 @@ namespace LSW {
 			}
 
 			template<typename T>
+			template<typename Q, typename Void, std::enable_if_t<!std::is_void_v<Q>&& std::is_void_v<Void>, int>>
+			Future<void> Future<T>::then(std::function<void(Void)> f) {
+				std::shared_ptr<Promise<void>> nuxt = std::make_shared<Promise<void>>();
+				Future<void> yee = nuxt->get_future();
+
+				_set_next([ff = std::move(f), nuu = std::move(nuxt)](){
+					ff();
+					nuu->set_value();
+				}, next);
+				return yee;
+			}
+
+			template<typename T>
 			template<typename R, std::enable_if_t<!std::is_void_v<R>, int>>
 			void Future<T>::_set_next(std::function<void(T)> f, std::shared_ptr<then_block<R>> bfor) {
 				if (bfor->has_future_task()) bfor->run_if_not_yet(*later_value.var, false);
@@ -288,9 +493,38 @@ namespace LSW {
 			/* * * * * * * * * * * * * * * * * * > PROMISE<T> < * * * * * * * * * * * * * * * * * */
 
 			template<typename T>
+			Promise<T>::Promise(Promise&& p) {
+				got_future = std::move(p.got_future);
+				task_to_do = std::move(p.task_to_do);
+				then_if = std::move(p.then_if);
+				got_future_once = std::move(p.got_future_once);
+				set_already_skip = std::move(p.set_already_skip);
+				std::promise<T>::operator=(std::move(p));
+			}
+
+			template<typename T>
+			void Promise<T>::operator=(Promise&& p) {
+				got_future = std::move(p.got_future);
+				task_to_do = std::move(p.task_to_do);
+				then_if = std::move(p.then_if);
+				got_future_once = std::move(p.got_future_once);
+				set_already_skip = std::move(p.set_already_skip);
+				std::promise<T>::operator=(std::move(p));
+			}
+
+			template<typename T>
+			Promise<T>::Promise(std::function<T(void)> nf) : std::promise<T>() {
+				task_to_do = nf;
+			}
+
+			template<typename T>
 			Future<T> Promise<T>::get_future() {
 				if (!got_future) got_future = true;
+#ifndef USE_STD_FULLY
+				else throw Handling::Abort(__FUNCSIG__, "For feature reasons, don't create multiple Futures from a single Promise", Handling::abort::abort_level::GIVEUP);
+#else
 				else throw std::exception("For feature reasons, don't create multiple Futures from a single Promise");
+#endif
 				Future<T> fut = std::future<T>(std::promise<T>::get_future());
 				then_if = fut._get_then();
 				return fut;
